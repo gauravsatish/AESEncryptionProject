5208d311d8af22289ac9119f8a614bd97de06833
Gaurav Satish <gauravsatish@proton.me>
2024-11-04 19:44:33 +0530

    refactor + exception handling
    still dogshit

--- AESImpl.py
+++ AESImpl.py
@@ -3,13 +3,14 @@ import hashlib
 from Crypto.Cipher import AES
 from Crypto.Util.Padding import pad, unpad
 
+
 class AESImpl:
     @staticmethod
     def encrypt(key, text):
-        cipher = AES.new(key, AES.MODE_ECB) # creating the encryption cipher
+        cipher = AES.new(key, AES.MODE_ECB)
 
-        byte_text = text.encode('utf-8') # Encode  UTF string to a byte string
-        pad_text = pad(byte_text, AES.block_size) # Padding the string to a multiple of 16 bytes
+        byte_text = text.encode("utf-8")
+        pad_text = pad(byte_text, AES.block_size)
 
         enc_text = cipher.encrypt(pad_text)
         return enc_text
@@ -19,12 +20,12 @@ class AESImpl:
         cipher = AES.new(key, AES.MODE_ECB)
         dec_text = cipher.decrypt(enc_text)
 
-        text = unpad(dec_text, AES.block_size) # removing the extra padding
-        text = text.decode('utf-8') # Converting back to UTF string from byte string
+        text = unpad(dec_text, AES.block_size)
+        text = text.decode("utf-8")
 
         return text
 
     @staticmethod
     def get_hashed_key(key):
-        key = hashlib.sha256(key.encode('utf-8')).hexdigest()[:16]
-        return key.encode('utf-8')
+        key = hashlib.sha256(key.encode("utf-8")).hexdigest()[:16]
+        return key.encode("utf-8")
--- main.py
+++ main.py
@@ -1,25 +1,21 @@
-#TODO: add exception handling
-from encrypter import existing_file_decrypt, existing_file_encrypt, new_message_encrypt
+# TODO: add exception handling
+from crypt import existing_file_decrypt, existing_file_encrypt, new_message_encrypt
 
-intro = """Welcome to message encryption and decryption
+print("""Welcome to message encryption and decryption
 Select an option:
-1. Encrypt
-2. Decrypt
-"""
-
-print(intro)
+    1. Encrypt
+    2. Decrypt
+""")
 
 choice = int(input())
 if choice == 1:
-    print("""Select an option
+    print("""\nSelect an option
     1. Encrypt an existing .txt file
     2. Create a new message in console and encrypt""")
     c = int(input())
     if c == 1:
         existing_file_encrypt()
-    elif c==2:
+    elif c == 2:
         new_message_encrypt()
-
 elif choice == 2:
     existing_file_decrypt()
-
